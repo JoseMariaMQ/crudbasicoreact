7a073cd7f0b319086128a36ef5e889cdfb850370
José María Muñoz Quijada <JoseMariaMQ@users.noreply.github.com>
2021-02-16 21:57:50 +0100

    Añadiendo Nombres

--- src/components/Listadonombres.jsx
+++ src/components/Listadonombres.jsx
@@ -1,6 +1,20 @@
 import React, {useState} from 'react'
+import uniqid from 'uniqid'
 
 const Listadonombres = () => {
+
+    const [nombre, setNombre] = useState('')
+    const [listanombres, setListanombres] = useState([])
+
+    const addNombre = (e) => {
+        e.preventDefault()
+        const nuevoNombre = {
+            id:uniqid(),
+            tituloNombre:nombre
+        }
+        setListanombres([...listanombres, nuevoNombre])
+    }
+
     return (
         <div>
             <h2>Aplicación de CRUD Básica</h2>
@@ -10,8 +24,8 @@ const Listadonombres = () => {
                 </div>
                 <div className="col">
                     <h2>Formulario para añadir nombres</h2>
-                    <form className="form-group">
-                        <input className="form-control mb-3" type="text" placeholder="Introduce el nombre"/>
+                    <form onSubmit={(e) => addNombre(e)} className="form-group">
+                        <input onChange={(e) => {setNombre(e.target.value)}} className="form-control mb-3" type="text" placeholder="Introduce el nombre"/>
                         <input className="btn btn-info btn-block" type="submit" value="Registrar Nombre"/>
                     </form>
                 </div>
